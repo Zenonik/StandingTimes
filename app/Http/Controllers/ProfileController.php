4867e8c13aa23d123b71bab1a320e9d0c18a6d92
Tobias Windmann <tobias.windmann@tricept.de>
2022-06-14 13:36:43 +0200

    ProfileController docs

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,12 +7,25 @@ use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
+    /**
+     * The index function returns the profile view with the user variable
+     *
+     * @return The user's profile page.
+     */
     public function index()
     {
         $user = auth()->user();
         return view('profile')->with(compact('user'));
     }
 
+    /**
+     * > It deletes the old avatar, uploads the new one, updates the name and password, and redirects the user to the
+     * profile page with a success message
+     *
+     * @param Request request The request object.
+     *
+     * @return The user's profile page with a success message.
+     */
     public function saveProfile(Request $request)
     {
         $user = auth()->user();
